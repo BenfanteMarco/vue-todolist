da0c619b075b8fa9aa26b64b964bc1bbae08631f
BenfanteMarco <145206234+BenfanteMarco@users.noreply.github.com>
2023-12-13 22:18:20 +0100

    added object's array

--- js/script.js
+++ js/script.js
@@ -3,7 +3,23 @@ const {createApp} = Vue;
 createApp({
     data(){
         return{
-
+            todoList: [
+                {
+                    text: 'fare colazione'
+                },
+                {
+                    text: 'studiare'
+                },
+                {
+                    text: 'andare in palestra'
+                },
+                {
+                    text: 'esercitarsi'
+                },
+                {
+                    text: 'riposare'
+                }
+            ]
         }
     }
 }).mount('#app');
